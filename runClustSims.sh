4c1a2c5d01848cd0d4cf7e249ceb3773a63a5416
Andrew Jahn <48071253+andrewjahn@users.noreply.github.com>
2019-07-29 11:09:10 -0400

    Update runClustSims.sh

--- runClustSims.sh
+++ runClustSims.sh
@@ -1,17 +1,25 @@
-  #!/bin/tcsh
+#!/bin/tcsh
   
-  setenv study $argv[1]
+setenv study $argv[1]
   
-  foreach meas (thickness volume)
-    foreach hemi (lh rh)
-      foreach smoothness (10)
-        foreach dir ({$hemi}.{$meas}.{$study}.{$smoothness}.glmdir)
+foreach meas (thickness volume)
+
+  foreach hemi (lh rh)
+    
+    foreach smoothness (10)
+      
+      foreach dir ({$hemi}.{$meas}.{$study}.{$smoothness}.glmdir)
+        
           mri_glmfit-sim \
-            --glmdir {$dir} \
-            --cache 1.3 pos \ #Change the vertex-wise threshold from 1.3 to 3.0 to maintain a 5% false positive rate. Also change "pos" to "abs" if you do not have an a priori hypothesis about the direction of your result
-            --cwp 0.05  \
-            --2spaces
-        end
+          --glmdir {$dir} \
+          --cache 1.3 pos \ #Change the vertex-wise threshold from 1.3 to 3.0 to maintain a 5% false positive rate. Also change "pos" to "abs" if you do not have an a priori hypothesis about the direction of your result
+          --cwp 0.05  \
+          --2spaces
+            
       end
+        
     end
+      
   end
+    
+end
